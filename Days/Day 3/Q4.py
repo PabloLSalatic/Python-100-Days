0fd3ebd3f756246087bbd60be70bc90ac03559a0
Pablo Lazo <pablolsalatic@gmail.com>
2021-04-21 18:16:53 -0300

    Terminado dia 3

--- Days/Day 3/Q4.py
+++ Days/Day 3/Q4.py
@@ -1,3 +1,4 @@
+
 # ?Write a program that accepts a sentence and calculate the number of letters and digits.
 
 # ?Suppose the following input is supplied to the program:
@@ -7,6 +8,13 @@
 # *LETTERS 10
 # *DIGITS 3
 
-# my_input = input('ingresar palabra y digitos: ')
-# TODO
+my_input = input('ingresar palabra y digitos: ')
+letter, digit = 0, 0
 
+for i in my_input:
+    if ('a' <= i and i <= 'z') or ('A' <= i and i <= 'Z'):
+        letter += 1
+    if '0' <= i and i <= '9':
+        digit += 1
+
+print(f'LETTERS: {letter} \n DIGITS: {digit}')
